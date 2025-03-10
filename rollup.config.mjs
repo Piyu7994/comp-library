8055b2e69c4dcd0b9dd6b1faf1dd357e93678a02
Piyush Kumar Pandey <piyush.pandey@practo.com>
2025-03-10 12:15:35 +0530

    chore: made similar node version

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,32 +4,32 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
-    input: 'src/index.tsx', // Entry file for your library
-    output: [
-      {
-        file: 'lib/commonjs/index.js',
-        format: 'cjs',
-        sourcemap: true
-      },
-      {
-        file: 'lib/module/index.js',
-        format: 'es',
-        sourcemap: true
-      }
-    ],
-    plugins: [
-      alias({
-        entries: [
-          { find: 'react-native', replacement: 'react-native-web' } // ✅ Alias react-native to react-native-web
-        ]
-      }),
-      resolve({
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      }),
-      commonjs(),
-      typescript({
-        tsconfig: 'tsconfig.json'
-      })
-    ],
-    external: ['react', 'react-native', 'react-native-web', 'react/jsx-runtime']
-  };
+  input: 'src/index.tsx', // Entry file for your library
+  output: [
+    {
+      file: 'lib/commonjs/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'lib/module/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    alias({
+      entries: [
+        { find: 'react-native', replacement: 'react-native-web' }, // ✅ Alias react-native to react-native-web
+      ],
+    }),
+    resolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    commonjs(),
+    typescript({
+      tsconfig: 'tsconfig.json',
+    }),
+  ],
+  external: ['react', 'react-native', 'react-native-web', 'react/jsx-runtime'],
+};
