5213a560ff94aa6c574dd103fba53632865d53ff
Piyush Kumar Pandey <piyush.pandey@practo.com>
2025-03-10 11:17:12 +0530

    chore: added support for react web

---
+++ rollup.config.mjs
@@ -0,0 +1,35 @@
+import alias from '@rollup/plugin-alias';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+    input: 'src/index.tsx', // Entry file for your library
+    output: [
+      {
+        file: 'lib/commonjs/index.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: 'lib/module/index.js',
+        format: 'es',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      alias({
+        entries: [
+          { find: 'react-native', replacement: 'react-native-web' } // ✅ Alias react-native to react-native-web
+        ]
+      }),
+      resolve({
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: 'tsconfig.json'
+      })
+    ],
+    external: ['react', 'react-native', 'react-native-web', 'react/jsx-runtime']
+  };
